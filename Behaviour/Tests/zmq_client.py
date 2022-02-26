8c302570aa26b7cb5ade22eb7bf8f5360a3ec7c5
marcdepape <marc.de.pape@gmail.com>
2022-02-26 12:02:53 -0500

    zmq client fix

--- Behaviour/Tests/zmq_client.py
+++ Behaviour/Tests/zmq_client.py
@@ -12,7 +12,7 @@ import threading
 
 # RPI HOSTNAME
 this_lamp = subprocess.check_output('hostname')
-this_lamp = this_lamp.replace("lamp","",1)
+this_lamp = this_lamp.replace("lamp","")
 
 # SERVER
 server_context = zmq.Context()
