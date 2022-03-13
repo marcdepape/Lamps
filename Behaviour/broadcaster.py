e927dbb056c51716dbc9812f19c891f021ce6299
marcdepape <marc.de.pape@gmail.com>
2022-03-12 19:35:21 -0500

    fix

--- Behaviour/broadcaster.py
+++ Behaviour/broadcaster.py
@@ -7,26 +7,26 @@ import alsaaudio
 from threading import Thread
 from time import sleep
 
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 22050
+CHUNK = 1024
+
+context = zmq.Context()
+mic_pub = context.socket(zmq.PUB)
+
+audio = pyaudio.PyAudio()
+
+def microphone(in_data, frame_count, time_info, status):
+    mic_pub.send(in_data)
+    return (None, pyaudio.paContinue)
+
+broadcast = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, stream_callback=microphone)
+
 class Broadcaster(object):
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 22050
-    CHUNK = 1024
-
-    audio = pyaudio.PyAudio()
-
-    context = zmq.Context()
-    mic_pub = context.socket(zmq.PUB)
-
     def __init__(self):
         self.is_broadcasting = False
         mic_pub.bind("tcp://*:8100")
-
-    def microphone(in_data, frame_count, time_info, status):
-        mic_pub.send(in_data)
-        return (None, pyaudio.paContinue)
-
-    broadcast = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, stream_callback=microphone)
 
     def start():
         broadcast.start_stream()
