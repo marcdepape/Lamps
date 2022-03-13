0d1b298e502e2ed5ea77b8fcfb6acc5530648f5d
marcdepape <marc.de.pape@gmail.com>
2022-03-13 15:06:40 -0400

    fix

--- Behaviour/Tests/lamp_pyaudio_client.py
+++ Behaviour/Tests/lamp_pyaudio_client.py
@@ -12,9 +12,10 @@ CHANNELS = 1
 RATE = 44100
 CHUNK = 4096
 
-audio = pyaudio.PyAudio()
 mixer = alsaaudio.Mixer()
 mixer.setvolume(100)
+
+is_streaming = False
 
 streams = [
     '192.168.100.193',
@@ -25,36 +26,63 @@ streams = [
     '192.168.100.117',
 ]
 
-id = 1
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((streams[id], 8100))
 
-def listener(in_data, frame_count, time_info, status):
-    data = s.recv(CHUNK)
-    return(data, pyaudio.paContinue)
+audio = pyaudio.PyAudio()
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK,)
 
-stream = audio.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True,
-                    frames_per_buffer=CHUNK,
-                    stream_callback=listener)
+volume = 0
+id = 1
 
-if __name__ == "__main__":
+def streaming():
+    state = True
+    while is_streaming:
+        if state:
+            print("NOW STREAMING")
+            state = False
+        data = s.recv(CHUNK)
+        stream.write(data)
+
+try:
+    is_streaming = False
+    s.connect((streams[id], 8100))
+    sleep(1)
+    is_streaming = True
+    print(streams[id])
+
+    audio = threading.Thread(target=streaming)
+    audio.start()
+
+    sleep(15)
+    id = id + 1
+
     while True:
         print("SWITCH TO: " + str(id))
-        stream.start_stream()
-        print(streams[id])
-        
-        sleep(15)
-
-        id = id + 1
-        if id > 5:
-            id = 1
-
+        is_streaming = False
+        audio.join()
+        print("THREAD CLOSED!")
         s.close()
-        stream.stop_stream()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sleep(1)
         s.connect((streams[id], 8100))
         sleep(1)
+        is_streaming = True
+        audio = threading.Thread(target=streaming)
+        audio.start()
+        print(streams[id])
+        sleep(15)
+        id = id + 1
+        if id > 5:
+            id = 1
+
+
+except KeyboardInterrupt:
+    while volume > 0:
+        volume -= 1
+        mixer.setvolume(volume)
+        sleep(0.5)
+
+print('Shutting down')
+s.close()
+stream.close()
+audio.terminate()
