b3b342cb015cdc6fbc091ba1c9afb1b27e6c5a0f
marcdepape <marc.de.pape@gmail.com>
2022-03-13 13:30:01 -0400

    fix

--- Behaviour/Tests/udp_server.py
+++ Behaviour/Tests/udp_server.py
@@ -17,26 +17,44 @@ def audio_stream_UDP():
     server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 
     server_socket.bind(('', 8100))
+
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
     CHUNK = 10*1024
+
     wf = wave.open("Behaviour/Tests/modular.wav")
     p = pyaudio.PyAudio()
     print('server listening at',(host_ip, (port)),wf.getframerate())
+
     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                     channels=wf.getnchannels(),
                     rate=wf.getframerate(),
                     input=True,
                     frames_per_buffer=CHUNK)
 
-    data = None
-    sample_rate = wf.getframerate()
+    def callback(in_data, frame_count, time_info, status):
+        server_socket.sendto(in_data,client_addr)
+        return (None, pyaudio.paContinue)
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK,
+                    stream_callback=callback)
+
+    #data = None
+    #sample_rate = wf.getframerate()
     while True:
         msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
         print('GOT connection from ',client_addr,msg)
 
         while True:
-            data = wf.readframes(CHUNK)
-            server_socket.sendto(data,client_addr)
-            time.sleep(0.8*CHUNK/sample_rate)
+            pass
+            #data = wf.readframes(CHUNK)
+            #server_socket.sendto(data,client_addr)
+            #time.sleep(0.8*CHUNK/RATE)
 
 
 
