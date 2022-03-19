cd4a4349732ea3ef956479334bdcc7591c138c5a
marcdepape <marc.de.pape@gmail.com>
2022-03-19 16:17:14 -0400

    fix

---
+++ Legacy/Bugs/audio_server/rtsp-server_lampx.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import Gst, GObject, GLib, GstRtspServer
+
+class RTSP_Server:
+    def __init__(self):
+        Gst.init(None)
+
+        self.server = GstRtspServer.RTSPServer.new()
+        self.address = '192.168.100.153'
+        self.port = '8554'
+
+        self.server.set_address(self.address)
+        self.server.set_service(self.port)
+
+        self.launch_description = "( filesrc location=05Arrows.ogg ! oggdemux ! queue ! rtpvorbispay name=pay0 pt=96 )"
+
+        '''
+        marc@armadillo:~$ gst-launch-1.0 rtspsrc location=rtsp://localhost:8554/mic ! queue ! rtpvorbisdepay ! vorbisdec ! audioconvert ! audio/x-raw,format=S16LE,channels=2 ! alsasink
+        '''
+
+        self.factory = GstRtspServer.RTSPMediaFactory.new()
+        self.factory.set_launch(self.launch_description)
+        self.factory.set_shared(True)
+        print(self.factory.get_launch())
+        self.mount_points = self.server.get_mount_points()
+        self.mount_points.add_factory('/mic', self.factory)
+
+        self.server.attach(None)
+        print('Stream ready: ' + str(self.server.get_address()))
+        GLib.MainLoop().run()
+
+
+server = RTSP_Server()
