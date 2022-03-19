57db02f8f04b7fa65cf32156501391161eba9861
marcdepape <marc.de.pape@gmail.com>
2022-03-19 13:44:55 -0400

    fix

--- Legacy/Bugs/audio_server/rtsp-server_localhost.py
+++ Legacy/Bugs/audio_server/rtsp-server_localhost.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import sys
 import gi
-#gi.require_version('Gst', '1.0')
-#gi.require_version('GstRtspServer', '1.0')
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GObject, GLib, GstRtspServer
 
 class RTSP_Server:
