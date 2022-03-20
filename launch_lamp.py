d1b29148b3468ac747446d4573ef81620738b6cd
marcdepape <marc.de.pape@gmail.com>
2022-03-20 14:54:41 -0400

    fix

--- launch_lamp.py
+++ launch_lamp.py
@@ -44,23 +44,23 @@ class Lamp(object):
         self.broadcast = None
 
 def launchServer():
-    self.server = GstRtspServer.RTSPServer.new()
-    self.address = 'lamp{}.local'.format(lamp_number)
-    self.port = '8100'
+    server = GstRtspServer.RTSPServer.new()
+    address = 'lamp{}.local'.format(lamp_number)
+    port = '8100'
 
-    self.server.set_address(self.address)
-    self.server.set_service(self.port)
-    self.launch_description = "( alsasrc ! queue leaky=downstream max-size-buffers=16 ! audio/x-raw,format=S16LE,rate=44100,channels=2 ! audioconvert ! vorbisenc quality=0.7 ! queue leaky=downstream max-size-buffers=16 ! rtpvorbispay name=pay0 pt=96 )"
+    server.set_address(address)
+    server.set_service(port)
+    launch_description = "( alsasrc ! queue leaky=downstream max-size-buffers=16 ! audio/x-raw,format=S16LE,rate=44100,channels=2 ! audioconvert ! vorbisenc quality=0.7 ! queue leaky=downstream max-size-buffers=16 ! rtpvorbispay name=pay0 pt=96 )"
 
-    self.factory = GstRtspServer.RTSPMediaFactory.new()
-    self.factory.set_launch(self.launch_description)
-    self.factory.set_shared(True)
-    print(self.factory.get_launch())
-    self.mount_points = self.server.get_mount_points()
-    self.mount_points.add_factory('/mic', self.factory)
+    factory = GstRtspServer.RTSPMediaFactory.new()
+    factory.set_launch(launch_description)
+    factory.set_shared(True)
+    print(factory.get_launch())
+    mount_points = server.get_mount_points()
+    mount_points.add_factory('/mic', factory)
 
-    self.server.attach(None)
-    print('Stream ready at: ' + str(self.server.get_address()))
+    server.attach(None)
+    print('Stream ready at: ' + str(server.get_address()))
     GLib.MainLoop().run()
 
 class RTSP_Server:
