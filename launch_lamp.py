603e49bb7c1078b92c17097e5e10641cd1784c29
marcdepape <marc.de.pape@gmail.com>
2022-03-20 20:36:06 -0400

    fix

--- launch_lamp.py
+++ launch_lamp.py
@@ -19,7 +19,7 @@ print("THIS LAMP IS LAMP NUMBER: " + this_lamp)
 lamp_id = int(this_lamp)
 
 streams = [
-    "://lamp0.local:8100",
+    "tcp://lamp0.local:8100",
     "tcp://lamp1.local:8100",
     "tcp://lamp2.local:8100",
     "tcp://lamp3.local:8100",
@@ -42,48 +42,6 @@ class Lamp(object):
         self.audio = None
         self.listen = None
         self.broadcast = None
-
-def launchServer():
-    server = GstRtspServer.RTSPServer.new()
-    address = 'lamp{}.local'.format(lamp_id)
-    port = '8100'
-
-    server.set_address(address)
-    server.set_service(port)
-    launch_description = "( alsasrc ! queue leaky=downstream max-size-buffers=16 ! audio/x-raw,format=S16LE,rate=44100,channels=2 ! audioconvert ! vorbisenc quality=0.7 ! queue leaky=downstream max-size-buffers=16 ! rtpvorbispay name=pay0 pt=96 )"
-
-    factory = GstRtspServer.RTSPMediaFactory.new()
-    factory.set_launch(launch_description)
-    factory.set_shared(True)
-    print(factory.get_launch())
-    mount_points = server.get_mount_points()
-    mount_points.add_factory('/mic', factory)
-
-    server.attach(None)
-    print('Stream ready at: ' + str(server.get_address()))
-    GLib.MainLoop().run()
-
-class RTSP_Server:
-    def __init__(self, lamp_number):
-
-        self.server = GstRtspServer.RTSPServer.new()
-        self.address = 'lamp{}.local'.format(lamp_number)
-        self.port = '8100'
-
-        self.server.set_address(self.address)
-        self.server.set_service(self.port)
-        self.launch_description = "( alsasrc ! queue leaky=downstream max-size-buffers=16 ! audio/x-raw,format=S16LE,rate=44100,channels=2 ! audioconvert ! vorbisenc quality=0.7 ! queue leaky=downstream max-size-buffers=16 ! rtpvorbispay name=pay0 pt=96 )"
-
-        self.factory = GstRtspServer.RTSPMediaFactory.new()
-        self.factory.set_launch(self.launch_description)
-        self.factory.set_shared(True)
-        print(self.factory.get_launch())
-        self.mount_points = self.server.get_mount_points()
-        self.mount_points.add_factory('/mic', self.factory)
-
-        self.server.attach(None)
-        print('Stream ready at: ' + str(self.server.get_address()))
-        GLib.MainLoop().run()
 
 class Streamer(object):
     def __init__(self):
@@ -127,9 +85,6 @@ class Streamer(object):
 
 Gst.init(None)
 streamer = Streamer()
-
-rtsp = Thread(target=launchServer)
-rtsp.start()
 
 if __name__ == "__main__":
     print("")
