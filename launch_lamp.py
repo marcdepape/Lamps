fc2a2e7130e7ce4f1ca1cc05968a9388573b5933
marcdepape <marc.de.pape@gmail.com>
2022-03-27 14:04:03 -0400

    fix

--- launch_lamp.py
+++ launch_lamp.py
@@ -82,6 +82,8 @@ class Lamp(object):
 
         if self.in_update["saturation"] != self.saturation:
             self.saturation = self.in_update["saturation"]
+            if self.state = "streaming":
+                setBulb(100)
 
         if self.in_update["stream"] != self.stream:
             self.stream = self.in_update["stream"]
