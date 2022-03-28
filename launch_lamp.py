2ff5f0f58ebc55944489238c52745a357cb8a20b
marcdepape <marc.de.pape@gmail.com>
2022-03-27 22:23:38 -0400

    fix

--- launch_lamp.py
+++ launch_lamp.py
@@ -184,6 +184,7 @@ class Streamer(object):
         while status == Gst.StateChangeReturn.ASYNC:
             status = self.pipeline.get_state(Gst.CLOCK_TIME_NONE)
             sleep(0.01)
+        print(status)
         if status == Gst.StateChangeReturn.SUCCESS:
             print("SUCCESS!")
         else:
