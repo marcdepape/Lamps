3c28f38d56dec259764abf1e84add54f4be8a83a
marcdepape <marc.de.pape@gmail.com>
2022-03-29 21:51:04 -0400

    fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Launching lamp processes!
-sudo python3 /home/pi/Projects/Lamps/launch_lamp.py &
+#sudo python3 /home/pi/Projects/Lamps/launch_lamp.py &
 python3 /home/pi/Projects/Lamps/launch_level.py &
 python3 /home/pi/Projects/Lamps/launch_rtsp.py &
