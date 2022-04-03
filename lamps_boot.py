29e28dd10a2e31e182a900d6f92d5366e633b911
marcdepape <marc.de.pape@gmail.com>
2022-04-02 22:44:51 -0400

    fix

--- lamps_boot.py
+++ lamps_boot.py
@@ -333,9 +333,6 @@ if __name__ == "__main__":
     rtsp = RTSP_Server(lamp_id)
     sleep(5)
 
-    os.system("python3 launch_mic.py &")
-    sleep(5)
-
     publisher = Thread(target=lamp.statusOut, args=())
     publisher.start()
     subscriber = Thread(target=lamp.updateIn, args=())
