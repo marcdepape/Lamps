2083548b203464f85ee24f37fa937b960f950b3c
marcdepape <marc.de.pape@gmail.com>
2022-04-23 11:17:11 -0400

    fix

--- Behaviour/Tests/alsa_test.py
+++ Behaviour/Tests/alsa_test.py
@@ -1,8 +1,11 @@
 import alsaaudio
 
+print(alsaaudio.cards())
+print(alsaaudio.mixers())
 pcms = alsaaudio.pcms(alsaaudio.PCM_CAPTURE)
 print("PCM: {}".format(alsaaudio.PCM_CAPTURE))
 print(pcms)
+pcm = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
 mixer = alsaaudio.Mixer()
 print(mixer.volumecap())
 print("RANGE: {}".format(mixer.getrange(alsaaudio.PCM_CAPTURE)))
