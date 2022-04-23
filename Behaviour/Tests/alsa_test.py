8c89265a1b661fc13e6d5a2dfd53f92b5078fece
marcdepape <marc.de.pape@gmail.com>
2022-04-23 11:10:49 -0400

    fix

--- Behaviour/Tests/alsa_test.py
+++ Behaviour/Tests/alsa_test.py
@@ -1,6 +1,7 @@
 import alsaaudio
 
 pcms = alsaaudio.pcms(alsaaudio.PCM_CAPTURE)
+print("PCM: {}".format(alsaaudio.PCM_CAPTURE))
 print(pcms)
 mixer = alsaaudio.Mixer()
 mixer.setvolume(100)[alsaaudio.PCM_CAPTURE]
