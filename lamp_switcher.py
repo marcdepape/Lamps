5a385d5d570c231579745e645b84d3ee00e72f7d
marcdepape <marc.de.pape@gmail.com>
2022-08-23 16:46:06 -0400

    fix

--- lamp_switcher.py
+++ lamp_switcher.py
@@ -4,12 +4,6 @@ import os
 from time import sleep, process_time, time, strftime
 
 #ssh-keygen -R 192.
-os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp0.local sudo ./launch_broadcast.sh &")
-#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp1.local sudo ./launch_lamp.sh &")
-#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp2.local sudo ./launch.sh &")
-#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp3.local sudo ./launch.sh &")
-#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp4.local sudo ./launch.sh &")
-#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp5.local sudo ./launch.sh &")
 
 # launch_broadcast.sh script
 '''
@@ -18,7 +12,32 @@ cd Projects/Lamps/
 git reset --hard
 git pull
 sudo alsactl restore -f alsa_lamps_codec.state
-sudo python3 Script/fase_out.py &
-sudo python3 Scripts/launch_server.py
-sudo python3 Scripts/pulse_mic.py &
+sudo python3 Scripts/launch_server.py &
 '''
+
+'''
+#!/bin/bash
+sudo killall -9 python3
+cd Projects/Lamps/
+git reset --hard
+git pull
+sudo alsactl restore -f alsa_lamps_codec.state
+sudo python3 Scripts/launch_stream.py --num $1 &
+'''
+
+os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp0.local sudo ./launch_broadcast.sh &")
+#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp1.local sudo ./launch_lamp.sh &")
+#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp2.local sudo ./launch.sh &")
+#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp3.local sudo ./launch.sh &")
+#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp4.local sudo ./launch.sh &")
+#os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp5.local sudo ./launch.sh &")
+
+
+if __name__ == '__main__':
+    while True:
+        os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp0.local sudo ./launch_broadcast.sh &")
+        os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp1.local sudo ./launch_stream 0.sh &")
+        sleep(60)
+        os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp1.local sudo ./launch_broadcast.sh &")
+        os.system("sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp0.local sudo ./launch_stream 1.sh &")
+        sleep(60)
