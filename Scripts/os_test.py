89958ceebcc428c158d4cf1b1869eaae3e5388b7
marcdepape <marc.de.pape@gmail.com>
2022-08-25 20:23:01 -0400

    fix

--- Scripts/os_test.py
+++ Scripts/os_test.py
@@ -3,4 +3,4 @@ import os
 
 #command = "cd ~ ; echo marcdepape | sudo -S ./launch_broadcast.sh"
 #os.system("echo marcdepape | sudo -S ; sudo ./launch_broadcast.sh &")
-os.system("ls")
+os.system("sudo ./relaunch_server.sh")
