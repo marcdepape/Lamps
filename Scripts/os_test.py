11d3d8b11783f0c10e08eeac13cdacbfb0b664c5
marcdepape <marc.de.pape@gmail.com>
2022-08-25 20:07:01 -0400

    fix

---
+++ Scripts/os_test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import os
+
+command = "cd ~ ; echo marcdepape | sudo -S ./launch_broadcast.sh"
+os.system(command)
---
+++ os_command.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import os
+
+command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp{}.local sudo python3 /Projects/Lamps/Scripts/is_test.py".format(0)
+os.system(command)
