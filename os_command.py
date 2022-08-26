64395863c3db63179f85e62af1c3404bd65b9a05
marcdepape <marc.de.pape@gmail.com>
2022-08-25 20:11:29 -0400

    fix

--- os_command.py
+++ os_command.py
@@ -4,5 +4,5 @@ import os
 command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp{}.local sudo ./pull.sh".format(0)
 os.system(command)
 
-command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp{}.local sudo python3 /Projects/Lamps/Scripts/is_test.py".format(0)
+command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no pi@lamp{}.local sudo python3 /Projects/Lamps/Scripts/os_test.py".format(0)
 os.system(command)
