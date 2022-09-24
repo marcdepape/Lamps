5b2ee1e994e7fec3f4c10f8fbdb251d73a140669
Marc De Pape <marc.de.pape@gmail.com>
2022-09-24 14:56:07 -0400

    fix

--- Scripts/fade_audio_out.py
+++ Scripts/fade_audio_out.py
@@ -1,5 +1,7 @@
 import alsaaudio
 from time import sleep
+
+'''
 import board
 from RPi import GPIO
 import neopixel
@@ -26,15 +28,18 @@ while value < 255:
     neo.show()
     value = value + 1
     sleep(0.001)
+'''
 
 while current_vol > 0:
     current_vol = current_vol - 1
     lineout.setvolume(current_vol)
     sleep(0.03)
 
+'''
 while value > 0:
     for i in range(0, num_pixels):
         neo[i] = (value,value,value)
     neo.show()
     value = value - 1
     sleep(0.001)
+'''
