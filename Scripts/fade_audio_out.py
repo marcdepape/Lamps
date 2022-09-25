650a3bf816a62e42eecc088e03196b145905044c
marcdepape <marc.de.pape@gmail.com>
2022-09-24 21:03:18 -0400

    fix

--- Scripts/fade_audio_out.py
+++ Scripts/fade_audio_out.py
@@ -1,6 +1,9 @@
 import alsaaudio
 from time import sleep
 
+lineout = alsaaudio.Mixer('Lineout')
+current_vol = int(lineout.getvolume()[0])
+
 while current_vol > 0:
     current_vol = current_vol - 1
     lineout.setvolume(current_vol)
