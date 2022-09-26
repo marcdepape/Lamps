c0f6d9c23d39768d8472da581b7ec1f2721a3fa0
marcdepape <marc.de.pape@gmail.com>
2022-09-25 21:09:06 -0400

    fix

--- Scripts/launch_server.py
+++ Scripts/launch_server.py
@@ -153,7 +153,7 @@ class ExtendedBin(Gst.Bin):
         elif message.type == Gst.MessageType.STATE_CHANGED:
             #pass
             oldState, newState, pendingState = message.parse_state_changed()
-            print ("State changed -> old:{}, new:{}, pending:{}".format(oldState.value, newState.value,pendingState.value))
+            print ("State changed -> old:{}, new:{}, pending:{}".format(oldState, newState,pendingState))
         elif message.type == Gst.MessageType.ELEMENT:
             structure = message.get_structure()
             name = structure.get_name()
