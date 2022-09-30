43e28fb5dd3144f72317b020f951e64a239706c6
marcdepape <marc.de.pape@gmail.com>
2022-09-29 22:47:59 -0400

    fix

--- ping.py
+++ ping.py
@@ -7,6 +7,9 @@ while True:
     response = os.system("ping -c 1 lamp{}.local".format(ping))
     if response != 0:
         print("LAMP {} IS OFFLINE!".format(ping))
+        while response != 0:
+            response = os.system("ping -c 1 lamp{}.local".format(ping))
+            sleep(0.01)
 
     ping = ping + 1
     if ping > 5:
