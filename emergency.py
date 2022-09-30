bc2d09b5dd20729d85b4718d93ef6f5ce15a9629
marcdepape <marc.de.pape@gmail.com>
2022-09-29 21:06:23 -0400

    fix

--- emergency.py
+++ emergency.py
@@ -5,5 +5,5 @@ import os
 number_of_lamps = 6
 
 for i in range(number_of_lamps):
-    command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no killall -9 python".format(i)
+    command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no killall 9 python3".format(i)
     os.system(command)
