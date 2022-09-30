217976e906d247f8c3cd7f137fa063b9c40e6a48
marcdepape <marc.de.pape@gmail.com>
2022-09-29 21:05:41 -0400

    fix

--- emergency.py
+++ emergency.py
@@ -2,6 +2,8 @@
 import random
 import os
 
+number_of_lamps = 6
+
 for i in range(number_of_lamps):
     command = "sshpass -p \'marcdepape\' ssh -o StrictHostKeyChecking=no killall -9 python".format(i)
     os.system(command)
