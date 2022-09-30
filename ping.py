55fd365834e8cf3aa0221bec5d7c8c6a344c45c1
marcdepape <marc.de.pape@gmail.com>
2022-09-29 22:29:33 -0400

    fix

---
+++ ping.py
@@ -0,0 +1,10 @@
+while True:
+    response = os.system("ping -c 1 lamp{}.local".format(ping))
+    if response != 0:
+        print("LAMP {} IS OFFLINE!".format(ping))
+
+    ping = ping + 1
+    if ping > 5:
+        ping = 0
+
+    sleep(1)
